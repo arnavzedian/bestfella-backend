ce62a5034aa2199ba5849aacb84e7f62db1d2b6d
Arnav Singh <36072753+itsarnavsingh@users.noreply.github.com>
2021-03-30 10:15:54 +0530

    minor bugs removed

---
+++ src/controllers/getDonationDetails.js
@@ -0,0 +1,15 @@
+let { Donation } = require("../../database/db");
+
+async function getFeeds(req, res, next) {
+  let donationID = req.body.donationID;
+
+  if (!donationID) return next("donationID field required");
+
+  let data = await Donation.findOne(
+    { _id: req.body.donationID },
+    { sort: { createdAt: -1 } }
+  );
+  return res.send({ data: data });
+}
+
+module.exports = getFeeds;
--- src/controllers/getFeeds.js
+++ src/controllers/getFeeds.js
@@ -2,11 +2,13 @@ let { Donation } = require("../../database/db");
 
 async function getFeeds(req, res, next) {
   let basis = req.body.basis;
-
+  let value = req.body.value;
   if (!basis) return next("basis field required");
   if (basis != "city" && basis != "donater") return next("invalid basis field");
 
-  let data = await Donation.find({ searchConfig }, { sort: { createdAt: -1 } });
+  let data = await Donation.find({ [basis]: value }, ["image", "title"], {
+    sort: { createdAt: -1 },
+  });
   return res.send({ data: data });
 }
 
